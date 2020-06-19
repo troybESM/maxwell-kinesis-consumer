712201fa80195294398d1c1e33edf5957bb792a5
epierce78 <epierce@hearsaycorp.com>
2020-06-19 15:15:13 -0400

    Support multiple (identical) targets for replication

---
+++ src/handlers/maxwell_kinesis_mysql.py
@@ -0,0 +1,36 @@
+import os
+import base64
+import json
+import logging
+from aws_kinesis_agg.deaggregator import deaggregate_records
+from src.consumers.mysql_consumer import MySQLConsumer
+from src.utils import get_secret
+
+logger = logging.getLogger()
+logger.setLevel(os.environ.get("LOG_LEVEL", "INFO"))
+
+
+def handle_event(event, context):
+    raw_records = event["Records"]
+    records = deaggregate_records(raw_records)
+    mysql = None
+    secret_string = None
+    i = 0
+
+    for record in records:
+        payload = json.loads(base64.b64decode(record["kinesis"]["data"]).decode()) # noqa
+        if secret_string is None:
+            try:
+                secret_string = get_secret("maxwell/{}".format(payload["database"])) # noqa
+            except Exception:
+                logger.warn("No secret found for table, ignoring")
+                return
+
+        if mysql is None:
+            mysql = MySQLConsumer(payload["database"], secret_string)
+            logger.info("Processing records for: {}".format(payload["database"])) # noqa
+        mysql.process_row(payload)
+        i = i + 1
+
+    logger.info("Number of records processed: {} ".format(str(i)))
+    mysql.close()
